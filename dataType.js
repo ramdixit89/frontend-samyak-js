696ac95e4f34f1aeb4e222a379ec6c0c67c08a5f
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-08-30 21:15:13 +0530

    code added

---
+++ dataType.js
@@ -0,0 +1,29 @@
+let name; // declaration
+name = "Ram Dixit"; // Initialization //String
+console.log(name);
+console.log(typeof name);
+
+let age ;
+age = 24;  //number
+
+console.log(age);
+console.log(typeof age);
+
+//Ya to true ya fir false
+let a = 5;
+let b = 5;
+let c = a == b;
+console.log(c);
+console.log(typeof c);   //boolean
+
+let z;
+console.log(z);
+console.log(typeof z); //undefined
+
+let x = null;
+console.log(x);
+console.log(typeof x);
+
+
+let num = BigInt("123456789012345678901234567890");
+console.log(num);
---
+++ operators.js
@@ -0,0 +1,5 @@
+//1: Arithmetic operators
+// +
+let a = 5;
+let b = 10;
+console.log("Addtion is :", a+b); 
