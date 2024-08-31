0c4d15be9ba6bacc86d6ef32b82f276dba84d67e
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-08-31 21:17:24 +0530

    code added

--- operators.js
+++ operators.js
@@ -1,5 +1,32 @@
 //1: Arithmetic operators
 // +
-let a = 5;
-let b = 10;
-console.log("Addtion is :", a+b); 
+// let a = 5;
+// let b = 10;
+// console.log("Addtion is :", a+b); 
+
+
+//Increment and Decrement operators
+
+// If used postfix, with operator after operand (for example, x-- ), the decrement operator decrements and returns the value before decrementing. If used prefix, with operator before operand (for example, --x ), the decrement operator decrements and returns the value after decrementing.
+
+// console.log("Increment : ", ++a);  //Prefix increment
+// console.log("Decrement : ", --a);  //Prefix decrement
+
+// console.log("Post Increment :", a++);  //Post Increment
+// console.log("Post Decrement : ", a--); //Post Decrement
+// console.log("", a--);
+// console.log("", a--);
+
+//2 : Comparision operator
+
+let x = 10;
+let  y = 12;
+
+console.log(x === y);
+console.log(x >= y);
+console.log(x <= y);
+// let name1 = "rajesh";
+// let name2 = "rajesh";
+
+// console.log(name1 >= name2);
+
