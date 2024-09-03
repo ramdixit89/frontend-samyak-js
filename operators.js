df96cb86108d58897909977df074367356781cb7
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-03 21:26:43 +0530

    code added

--- operators.js
+++ operators.js
@@ -19,14 +19,49 @@
 
 //2 : Comparision operator
 
-let x = 10;
-let  y = 12;
+// let x = 10;
+// let  y = 12;
 
-console.log(x === y);
-console.log(x >= y);
-console.log(x <= y);
+// console.log(x === y);
+// console.log(x >= y);
+// console.log(x <= y);
 // let name1 = "rajesh";
 // let name2 = "rajesh";
 
 // console.log(name1 >= name2);
+
+//3 : Bitwise Operator
+
+//Voting
+
+// let age = prompt("Enter age of a person :");
+// let age = 18;
+
+// if(age > 18 & age >= 18){
+// //true
+// alert("Person is eligible for vote");
+// }
+// else{
+// //false
+// alert("Person is not eligible for vote");
+// console.log("Not eleigible");
+// }
+
+
+//4 : Assignment Operators
+// let a = 5;
+// let b = 10;
+// let c = a %= b;
+
+// console.log("Addition is : ",c);
+// alert("Addition is : "+c);
+
+
+//5: Special Operators
+
+let age = 15;
+
+let result = age >= 18 ? "Eligibile" : "Not eleigible";
+console.log(result);
+
 
