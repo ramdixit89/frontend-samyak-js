b6fb00c566a3388b61ee41b54edcb585ab418055
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-04 21:14:27 +0530

    code added

---
+++ conditionalStatement.js
@@ -0,0 +1,28 @@
+//1 : if(){ 
+//}else { }
+
+// let hour =  18;
+// if(hour  < 18){
+//     //block of code
+//     let greeting = "Good day"; //statement
+//     console.log(greeting); //statement
+// }else{
+//     let greeting =  "Good night";
+//     console.log(greeting);
+// }
+
+// let num = 10;
+// if(num % 2 == 0){
+//    console.log("Number is even number");
+// }else{
+//     console.log("Number is not divisible by 2");
+// }
+
+
+// let num2 = 13;
+// if(num2 % 2 != 0){
+//    console.log("Not divisible by 2");
+// }else{
+//    console.log("divisible by 2");
+// }
+
