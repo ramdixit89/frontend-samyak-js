8e86220fd685f1574bc2d0a269b3939402872b29
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-05 21:26:13 +0530

    code added

--- conditionalStatement.js
+++ conditionalStatement.js
@@ -26,3 +26,40 @@
 //    console.log("divisible by 2");
 // }
 
+//2 : else if
+
+// let time = 10;            
+// if(time < 20){
+//     console.log("Good morning");      
+// }else if(time > 40){
+//     console.log("Midnight");
+// } 
+// else if(time > 20){
+//   console.log("Good day");
+// } 
+// else{
+//     console.log("Good evening");  //False
+// }
+
+
+
+let num1 = parseInt(prompt("Enter number 1"));
+let num2 = parseInt(prompt("Enter number 2"));
+let operator = parseInt(prompt("Enter operator"));
+// let num1 = 10;
+// let num2 = 20;
+let result;
+// let operator = "*";
+if(operator == "+"){
+    result = num1 + num2;
+    console.log("Addition is :",result);
+}else if(operator == "-"){
+    result = num1 - num2;
+    console.log("Substraction is : ",result);
+}else if(operator == "/"){
+    result = num1/num2;
+    console.log("Division is : ", result);
+}else if(operator == "*"){
+    result = num1 * num2;
+    console.log("Multiplication is : ", result);
+}
