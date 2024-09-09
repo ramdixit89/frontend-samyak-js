74140922431b35645baf60f7e901eaf7c9107a92
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-09 21:32:44 +0530

    code commit

--- conditionalStatement.js
+++ conditionalStatement.js
@@ -43,23 +43,36 @@
 
 
 
-let num1 = parseInt(prompt("Enter number 1"));
-let num2 = parseInt(prompt("Enter number 2"));
-let operator = parseInt(prompt("Enter operator"));
-// let num1 = 10;
-// let num2 = 20;
-let result;
-// let operator = "*";
-if(operator == "+"){
-    result = num1 + num2;
-    console.log("Addition is :",result);
-}else if(operator == "-"){
-    result = num1 - num2;
-    console.log("Substraction is : ",result);
-}else if(operator == "/"){
-    result = num1/num2;
-    console.log("Division is : ", result);
-}else if(operator == "*"){
-    result = num1 * num2;
-    console.log("Multiplication is : ", result);
+// let num1 = parseInt(prompt("Enter number 1"));
+// let num2 = parseInt(prompt("Enter number 2"));
+// let operator = parseInt(prompt("Enter operator"));
+// // let num1 = 10;
+// // let num2 = 20;
+// let result;
+// // let operator = "*";
+// if(operator == "+"){
+//     result = num1 + num2;
+//     console.log("Addition is :",result);
+// }else if(operator == "-"){
+//     result = num1 - num2;
+//     console.log("Substraction is : ",result);
+// }else if(operator == "/"){
+//     result = num1/num2;
+//     console.log("Division is : ", result);
+// }else if(operator == "*"){
+//     result = num1 * num2;
+//     console.log("Multiplication is : ", result);
+// }
+
+
+let studentName = "Ram";
+let studentRolllNo = 10;
+let stuPercentage = 40;
+
+if( stuPercentage < 33 ){
+    console.log("Fail");
+} else if( stuPercentage <= 60 && stuPercentage >= 33 ){
+   console.log("C Grade");
+} else if( stuPercentage < 75 && stuPercentage > 60 ){
+        
 }
