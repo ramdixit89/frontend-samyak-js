05e1ae94a809f6db93a74997515fcf1dce7a6107
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-11 22:14:24 +0530

    code added

---
+++ loops.js
@@ -0,0 +1,37 @@
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+
+// let i = 0;
+// if(i<5)
+// {
+//    console.log(i++);
+// }
+
+// 1: for( initialization ; condition ; increment/decrement){ 
+//}
+
+// for(let num = 100; num >= 0;num--){
+//     console.log(num);
+// }
+
+//print table
+
+// let num = 3;
+
+// for(let i = 1; i <= 10; i++){
+//     let result = i*num;
+//     console.log("Table is ", result);
+// }
+
+//2 : while loop
+
+let num = 1;  //Initialization hua hai
+let number1 = 2;
+while(num <= 10){  //Condition
+   console.log(num*number1);
+   num++; //Increment
+}
