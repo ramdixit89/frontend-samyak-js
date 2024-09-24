2f965814c055601302c0e504294fb3222487f3cf
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-09-24 21:10:03 +0530

    code added
    Functions in JavaSCript

---
+++ function.js
@@ -0,0 +1,92 @@
+ //Arguments
+// //1 : Function Declaration :
+// function add(){ 
+//     //2: Function Defination or Function body
+//     let a = 5;
+//     let b = 5;
+//     let sum = a + b;
+//     console.log(sum);
+// }
+// //3 : function calling
+// add();
+
+// function sub(){
+//     let a = 2;
+//     let b = 5;
+//     let sub = a - b;
+//     console.log(sub);
+// }
+// sub();
+
+//Parameter and arguments :
+// add("5", 6); 
+// function add(a, b){ //Parameters
+//    let sum = parseInt(a) + parseInt(b);
+//    console.log(sum);
+// }
+
+//ATM Transaction using funtions in javascript
+
+//Deposite
+// function deposit(balance, amount){
+//     let deposit = balance + amount;
+//     console.log("Amount deposited : ",deposit);
+// }
+// deposit(3000, 1000);
+
+// //Withdrawal
+// function withdraw(balance, amount){
+//     let withdraw = balance - amount;
+//     console.log("Amount withdraw : ",withdraw);
+// }
+// withdraw(3000, 1000);
+
+// //Check balance
+// function checkBalance(balance){
+//     console.log("Your Balance is : ",balance);
+// }
+// checkBalance(3000);
+
+// Types of Function Declaration
+// 1 : Function Expression
+// 2 : Arrow function
+// 3 : IIFE (Immediately Invoked Function Expression)
+
+// let add= function(a,b){
+//   let sum = a + b;
+//   console.log(sum);
+// }
+// add(5, 5);
+
+// let sub  = function(a,b){
+//   let sub = a - b;
+//   console.log(sub);
+// }
+// sub(5, 5);
+
+//2 :  Arrow function
+// let add = (a, b) =>{ 
+//     let sum = a + b;
+//     // return sum;  
+//     console.log(sum);
+// }
+// add(5, 5);
+// let newAdd = add(5, 5);
+// console.log(newAdd);
+// (2,5);
+// (
+//     function add(a,b){
+//       console.log("Hello");
+//       console.log(a);
+//       console.log(b);
+//     }
+// )
+// (2, 4);  //Calling
+
+// (
+//     function sub(a , b){
+//       console.log("Hello");
+//       console.log(a);
+//       console.log(b);
+//     }
+// )
