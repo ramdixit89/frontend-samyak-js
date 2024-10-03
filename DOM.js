a7f697a8093a8ee1de179164ee15a6706c1c7780
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-10-03 21:03:46 +0530

    code added

--- DOM.js
+++ DOM.js
@@ -1,1 +1,19 @@
-document.getElementById('changeContent').innerHTML = "JavaScript";
+//Methods in DOM
+//1. getElementById()
+//2. getElementsByClassName()
+//3. getElementsByTagName()
+//4. querySelector()
+//5. querySelectorAll()
+//6. innerHTML
+//7. innerText
+//8. textContent
+// document.getElementById('head1').innerHTML = 'Hello World';
+// document.getElementsByClassName('para')[0].innerHTML = "This is a paragraph";
+// document.getElementsByClassName('para')[1].innerHTML = "2nd paragraph";
+// // console.log(document.getElementsByClassName('para'));
+// document.getElementsByTagName('span')[0].innerHTML = "This is a span";
+// document.getElementsByTagName('span')[1].innerHTML = "This is a span 2";
+
+// //querySelector()
+// document.querySelector('.para').innerHTML = "THis is a heading";
+// document.querySelector('span').innerHTML = "changed para";
