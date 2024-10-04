8bce2e02451dcea2e0c7406112878ff31de2c13c
Ram Dixit <155820005+ramdixit89@users.noreply.github.com>
2024-10-04 20:19:22 +0530

    code added

--- DOM.js
+++ DOM.js
@@ -7,6 +7,7 @@
 //6. innerHTML
 //7. innerText
 //8. textContent
+//9.createElement()
 // document.getElementById('head1').innerHTML = 'Hello World';
 // document.getElementsByClassName('para')[0].innerHTML = "This is a paragraph";
 // document.getElementsByClassName('para')[1].innerHTML = "2nd paragraph";
@@ -17,3 +18,29 @@
 // //querySelector()
 // document.querySelector('.para').innerHTML = "THis is a heading";
 // document.querySelector('span').innerHTML = "changed para";
+
+//console.log(document.querySelectorAll('#head1')[0].innerHTML = "This is a heading");
+
+// let para = document.querySelectorAll('#head1');
+// for(let i = 0; i <= para.length; i++){
+//    para[i].style.border = "1px solid black";
+//    console.log(para[i]);
+// }
+// para[0].innerHTML = "This is a heading";
+// para[0].style.backgroundColor = 'red';
+// para[0].style.textAlign = 'center';
+// para[0].style.color = "Blue";
+// para[1].style.backgroundColor = 'red';
+
+
+//createElement()
+// let container = document.createElement('div');
+// container.innerHTML = "This is a container";
+// container.style.backgroundColor = 'red';
+// document.body.appendChild(container);
+
+// let para = document.createElement('p');
+// para.innerHTML = "This is a paragraph";
+// container.appendChild(para);
+
+
